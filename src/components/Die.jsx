d822dfc328f85de932ddb07fc1e6be964ac3575b
Nischal Khadka <justchalchal@gmail.com>
2022-12-02 11:21:30 +0545

    less ugly

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,43 +1,40 @@
-import {TbDice1, TbDice2, TbDice3, TbDice4, TbDice5, TbDice6} from 'react-icons/tb'
-export default function Die(props){
-    const styles = {
-        backgroundColor: props.isHeld ? "#336699" : "white"
+import {
+  TbDice1,
+  TbDice2,
+  TbDice3,
+  TbDice4,
+  TbDice5,
+  TbDice6,
+} from "react-icons/tb";
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#336699" : "white",
+  };
+
+  function images(a) {
+    if (a === 1) {
+      return <TbDice1 className="dice-image" />;
     }
-
-    
-
-    function images(a){
-        if (a === 1 ){
-            return(
-                <TbDice1 className='dice-image'/>
-            )
-        } if (a ===2){
-            return(
-                <TbDice2 className='dice-image'/>
-            )
-        } if (a ===3){
-            return(
-                <TbDice3 className='dice-image'/>
-            )
-        }if (a ===4){
-            return(
-                <TbDice4 className='dice-image'/>
-            )
-        }if (a ===5){
-            return(
-                <TbDice5 className='dice-image'/>
-            )
-        }if (a ===6){
-            return(
-                <TbDice6 className='dice-image'/>
-            )
-        }   
+    if (a === 2) {
+      return <TbDice2 className="dice-image" />;
     }
+    if (a === 3) {
+      return <TbDice3 className="dice-image" />;
+    }
+    if (a === 4) {
+      return <TbDice4 className="dice-image" />;
+    }
+    if (a === 5) {
+      return <TbDice5 className="dice-image" />;
+    }
+    if (a === 6) {
+      return <TbDice6 className="dice-image" />;
+    }
+  }
 
-    return(
-        <div className="dice-face" style={styles} onClick={props.onClick}>
-            <h1>{images(props.value)}</h1>
-        </div>
-    )
-
+  return (
+    <div className="dice-face" style={styles} onClick={props.onClick}>
+      <h1>{images(props.value)}</h1>
+    </div>
+  );
 }
